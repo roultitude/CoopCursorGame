29db769eb6f47e130bcc7e9186b4d4b6ddcb9032
roultitude <51879825+roultitude@users.noreply.github.com>
2024-11-05 22:40:31 +0800

    Added enemy spawning indicators

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -1,48 +1,64 @@
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
 public class EnemySpawner : NetworkBehaviour
 {
     [SerializeField] NetworkObject[] enemyPrefabs;
-    [SerializeField]
-    Transform[] spawnTransforms;
+    [SerializeField] EnemySpawnIndicator enemySpawnIndicatorPrefab;
+    [SerializeField] float spawnIndicatorTime;
+
+    private Queue<(Vector2 location, NetworkObject enemyPrefab)> spawnQueue 
+        = new Queue<(Vector2 location, NetworkObject enemyPrefab)>();
     
+
     public override void OnNetworkSpawn()
     {
         if (!IsServer) return;
-        Invoke("SpawnEnemy", 5);
-        Invoke("SpawnEnemy", 6);
-        Invoke("SpawnEnemy", 7);
+        Invoke(nameof(CreateEnemy), 5);
+        Invoke(nameof(CreateEnemy), 6);
+        Invoke(nameof(CreateEnemy), 7);
 
 
-        Invoke("SpawnEnemy", 25);
-        Invoke("SpawnEnemy", 26);
-        Invoke("SpawnEnemy", 27);
-        Invoke("SpawnEnemy", 28);
-        Invoke("SpawnEnemy", 29);
+        Invoke(nameof(CreateEnemy), 25);
+        Invoke(nameof(CreateEnemy), 26);
+        Invoke(nameof(CreateEnemy), 27);
+        Invoke(nameof(CreateEnemy), 28);
+        Invoke(nameof(CreateEnemy), 29);
 
-        Invoke("SpawnEnemy", 51);
-        Invoke("SpawnEnemy", 52);
-        Invoke("SpawnEnemy", 53);
-        Invoke("SpawnEnemy", 54);
-        Invoke("SpawnEnemy", 55);
-        Invoke("SpawnEnemy", 56);
-        Invoke("SpawnEnemy", 57);
-        Invoke("SpawnEnemy", 58);
+        Invoke(nameof(CreateEnemy), 51);
+        Invoke(nameof(CreateEnemy), 52);
+        Invoke(nameof(CreateEnemy), 53);
+        Invoke(nameof(CreateEnemy), 54);
+        Invoke(nameof(CreateEnemy), 55);
+        Invoke(nameof(CreateEnemy), 56);
+        Invoke(nameof(CreateEnemy), 57);
+        Invoke(nameof(CreateEnemy), 58);
         //InvokeRepeating("SpawnEnemy", 10,5);
     }
 
-
+    [Rpc(SendTo.Everyone)]
+    public void CreateEnemySpawnIndicatorRPC(float x, float y, float spawnTime)
+    {
+        EnemySpawnIndicator esi = Instantiate(enemySpawnIndicatorPrefab, new Vector3(x,y),Quaternion.identity);
+        esi.Setup(spawnTime);
+    }
     
+    public void CreateEnemy()
+    {
+        if (!IsServer) return; // only server can spawn enemies
+        Vector2 loc = new Vector2(Random.Range(-8.5f, 8.5f), Random.Range(-5, 5));
+        CreateEnemySpawnIndicatorRPC(loc.x,loc.y, spawnIndicatorTime);
+        spawnQueue.Enqueue((loc, enemyPrefabs[Random.Range(0, enemyPrefabs.Length)]));
+        Invoke(nameof(SpawnEnemy),spawnIndicatorTime);
+    }
+
     public void SpawnEnemy()
     {
         Debug.Log("Attempting to Spawn Enemy");
-        if (!IsServer) return; // only server can spawn enemies
-
         //server instantiates
-        NetworkObject netObj = Instantiate(enemyPrefabs[Random.Range(0,enemyPrefabs.Length)], 
-            spawnTransforms[Random.Range(0, spawnTransforms.Length)].position, 
-            Quaternion.identity);
+        (Vector2 loc, NetworkObject enemyPrefab) = spawnQueue.Dequeue();
+        NetworkObject netObj = Instantiate(enemyPrefab, loc, Quaternion.identity);
         //call spawn to propagate spawn to clients
         netObj.Spawn(true);
     }

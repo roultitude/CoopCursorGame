8aa5889c3d1fafdddd6a2b23dd51015b47b9ec27
roultitude <51879825+roultitude@users.noreply.github.com>
2024-12-29 10:51:06 +0800

    import BulletPro

---
+++ Assets/BulletPro/ExampleAssets/Graphics/Shared/CommonAnimations/SimpleAlphaAnimation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is supported by the BulletPro package for Unity.
+// Template author : Simon Albou <albou.simon@gmail.com>
+
+namespace BulletPro.DemoScripts
+{
+	public class SimpleAlphaAnimation : MonoBehaviour
+	{
+		public AnimationCurve animCurve = AnimationCurve.Constant(0, 1, 1);
+		public float period = 2f;
+		public SpriteRenderer spriteRenderer;
+		float age;
+
+		void Update()
+		{
+			age += Time.deltaTime;
+			float ratio = (age % period)/period;
+
+			spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, animCurve.Evaluate(ratio));
+		}	
+	}
+}
